817219d671084a0c901ca6124d50549ac52fc9fb
Ifeanyi Chima <chimaifeanyi29@gmail.com>
2025-04-07 12:28:59 -0700

    add useState

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 
 const User = () => {
 
+    const [username, setUsername] = useState("");
+    const [userData, setUserData] = useState(null);
+    const [error, setError] = useState("");
 
 
-    
+
   return (
     <div>User</div>
   )
